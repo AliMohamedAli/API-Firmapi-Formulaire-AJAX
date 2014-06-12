813f9100d3bea251b3fa99171974b1e50c152bf6
devandclick <devandclick@gmail.com>
2014-06-12 18:18:44 +0200

    Apprendre l'ajax facilement

--- api.php
+++ api.php
@@ -1,2 +1,2 @@
 <?php
-echo @file_get_contents('https://firmapi.com/api/v1/company?siren='.$_GET['siren'].'&api_key=14588e79cc9c7fdb8143d3c1bf1c47b3d34afa03');
+echo @file_get_contents('https://firmapi.com/api/v1/company?siren='.$_GET['siren'].'&api_key={KEY}'); //Key à compléter par votre clé d'API
